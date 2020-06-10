8883b6fa665bd0c17e4887c0c8efd58ad47bde66
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2020-06-10 19:08:11 -0400

    Carpet extension loading fix

--- src/main/java/mini_tweaks/mixins/MinecraftClient_NoopMixin.java
+++ src/main/java/mini_tweaks/mixins/MinecraftClient_NoopMixin.java
@@ -6,16 +6,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import mini_tweaks.MiniTweaks;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.main.Main;
 
-@Mixin(MinecraftClient.class)
+@Mixin(Main.class)
 public class MinecraftClient_NoopMixin {
     // this is here just to load the ExampleExtension class, otherwise noone would load it / need it
     // if you have already you own mixins that use your extension class in any shape or form
     // you don't need this one
     // This one is for the client
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void loadMe(CallbackInfo ci) {
+    @Inject(method = "main", at = @At("HEAD"))
+    private static void loadMe(String[] args, CallbackInfo ci) {
         MiniTweaks.noop();
     }
 }
--- src/main/java/mini_tweaks/mixins/MinecraftServer_NoopMixin.java
+++ src/main/java/mini_tweaks/mixins/MinecraftServer_NoopMixin.java
@@ -6,16 +6,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import mini_tweaks.MiniTweaks;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.Main;
 
-@Mixin(MinecraftServer.class)
+@Mixin(Main.class)
 public class MinecraftServer_NoopMixin {
     // this is here just to load the ExampleExtension class, otherwise noone would load it / need it
     // if you have already you own mixins that use your extension class in any shape or form
     // you don't need this one
     // You need this one to run a server properly
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void loadMe(CallbackInfo ci) {
+    @Inject(method = "main", at = @At("HEAD"))
+    private static void loadMe(String[] args, CallbackInfo ci) {
         MiniTweaks.noop();
     }
 }
