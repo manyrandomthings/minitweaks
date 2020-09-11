79110194b4c2fdf10297f5edc9f2dbd93ec3c656
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2020-09-11 08:00:05 -0400

    disableBlazeFire improvements

--- src/main/java/mini_tweaks/mixins/SmallFireballEntity_FireMixin.java
+++ src/main/java/mini_tweaks/mixins/SmallFireballEntity_FireMixin.java
@@ -2,21 +2,20 @@ package mini_tweaks.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import mini_tweaks.MiniTweaksSettings;
 import net.minecraft.entity.projectile.SmallFireballEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.util.hit.BlockHitResult;
 
 @Mixin(SmallFireballEntity.class)
 public class SmallFireballEntity_FireMixin {
-    @Redirect(method = "onBlockHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isAir(Lnet/minecraft/util/math/BlockPos;)Z"))
-    private boolean preventFire(World world, BlockPos pos) {
+    @Inject(method = "onBlockHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/AbstractFireballEntity;onBlockHit(Lnet/minecraft/util/hit/BlockHitResult;)V", shift = At.Shift.AFTER), cancellable = true)
+    private void preventFire(BlockHitResult blockHitResult, CallbackInfo ci) {
         if(MiniTweaksSettings.disableBlazeFire) {
-            // trick into thinking the block isn't air so it doesn't place fire
-            return false;
+            // cancel if blaze fire is disabled
+            ci.cancel();
         }
-        return world.isAir(pos);
     }
 }
