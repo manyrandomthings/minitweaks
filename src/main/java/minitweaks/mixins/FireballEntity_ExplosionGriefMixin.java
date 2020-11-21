b00142427cc720a4ff1eccfcecc652ebcb318086
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2020-11-21 18:36:36 -0500

    Split up ghastBlockDamage and disableGhastFire mixins

--- src/main/java/minitweaks/mixins/FireballEntity_ExplosionGriefMixin.java
+++ src/main/java/minitweaks/mixins/FireballEntity_ExplosionGriefMixin.java
@@ -2,24 +2,21 @@ package minitweaks.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import minitweaks.MiniTweaksSettings;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.FireballEntity;
-import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
 import net.minecraft.world.explosion.Explosion.DestructionType;
 
 @Mixin(FireballEntity.class)
 public class FireballEntity_ExplosionGriefMixin {
-    @Redirect(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
-    private Explosion modifyFireballExplosion(World world, Entity entity, double x, double y, double z, float power, boolean createFire, DestructionType destructionType) {
-        // get disableGhastFire option for disabling fire
-        boolean modifiedCreateFire = !MiniTweaksSettings.disableGhastFire;
-        // get destruction type for ghastFireballBlockDamage
-        DestructionType modifiedDestructionType = MiniTweaksSettings.getExplosionType(MiniTweaksSettings.ghastBlockDamage, destructionType);
+    @ModifyArg(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
+    private DestructionType modifyDestructionType(DestructionType destructionType) {
+        return MiniTweaksSettings.getExplosionType(MiniTweaksSettings.ghastBlockDamage, destructionType);
+    }
 
-        return world.createExplosion(entity, x, y, z, power, modifiedCreateFire, modifiedDestructionType);
+    @ModifyArg(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
+    private boolean modifyCreateFire(boolean createFire) {
+        return createFire && !MiniTweaksSettings.disableGhastFire;
     }
 }
