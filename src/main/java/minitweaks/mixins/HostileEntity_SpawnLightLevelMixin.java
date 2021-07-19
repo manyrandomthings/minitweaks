d07c05cacac39bf98b041ce89869d7936570da44
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2021-07-19 02:36:43 -0400

    1.18 completeDarknessSpawning rule

---
+++ src/main/java/minitweaks/mixins/HostileEntity_SpawnLightLevelMixin.java
@@ -0,0 +1,24 @@
+package minitweaks.mixins;
+
+import java.util.Random;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import minitweaks.MiniTweaksSettings;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
+import net.minecraft.world.ServerWorldAccess;
+
+@Mixin(HostileEntity.class)
+public class HostileEntity_SpawnLightLevelMixin {
+    @Inject(method = "isSpawnDark", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ServerWorldAccess;toServerWorld()Lnet/minecraft/server/world/ServerWorld;"), cancellable = true)
+    private static void lightLevelModify(ServerWorldAccess world, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+        if(MiniTweaksSettings.completeDarknessSpawning && world.getLightLevel(LightType.BLOCK, pos) > 0) {
+            cir.setReturnValue(false);
+        }
+    }
+}
