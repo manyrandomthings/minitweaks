602a838566448b3aad3729a4d4919c1985957eb5
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2022-03-27 17:24:56 -0400

    1.18.2

--- src/main/java/minitweaks/mixins/PhantomSpawner_SpawnTimeMixin.java
+++ src/main/java/minitweaks/mixins/PhantomSpawner_SpawnTimeMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import minitweaks.MiniTweaksSettings;
-import net.minecraft.world.gen.PhantomSpawner;
+import net.minecraft.world.spawner.PhantomSpawner;
 
 @Mixin(PhantomSpawner.class)
 public abstract class PhantomSpawner_SpawnTimeMixin {
