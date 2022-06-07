abff2cf7e15d926df42a88359f322ec64612763f
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2022-06-07 15:37:32 -0400

    fix #22

--- src/main/java/minitweaks/mixins/mob/sheep/dye/DyeItemMixin.java
+++ src/main/java/minitweaks/mixins/mob/sheep/dye/DyeItemMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(DyeItem.class)
+@Mixin(value = DyeItem.class, priority = 1001)
 public class DyeItemMixin {
     @Redirect(method = "useOnEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/SheepEntity;isSheared()Z"))
     private boolean alwaysDyeSheep(SheepEntity sheepEntity) {
