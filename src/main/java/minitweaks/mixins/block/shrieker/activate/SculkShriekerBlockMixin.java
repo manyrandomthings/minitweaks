0ac9b142d606c1386e462a819caef8e61b286d45
manyrandomthings <42223559+manyrandomthings@users.noreply.github.com>
2024-06-01 07:11:49 -0400

    1.20.6, fix crash with echoShardsEnableShriekers

--- src/main/java/minitweaks/mixins/block/shrieker/activate/SculkShriekerBlockMixin.java
+++ src/main/java/minitweaks/mixins/block/shrieker/activate/SculkShriekerBlockMixin.java
@@ -8,8 +8,10 @@ import net.minecraft.block.SculkShriekerBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -19,28 +21,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SculkShriekerBlock.class)
 public abstract class SculkShriekerBlockMixin extends AbstractBlockMixin {
     @Override
-    protected void onUseWithItemInject(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        // if rule is enabled
-        if(MiniTweaksSettings.echoShardsEnableShriekers) {
-            // check if item is echo shard and being used on a sculk shrieker
-            if(stack.isOf(Items.ECHO_SHARD) && state.isOf(Blocks.SCULK_SHRIEKER)) {
-                // check if shrieker can already summon wardens
-                if(!state.get(SculkShriekerBlock.CAN_SUMMON)) {
-                    // remove echo shard if in survival
-                    if(!player.getAbilities().creativeMode) {
-                        stack.decrement(1);
-                    }
-                    // set can_summon state to true
-                    world.setBlockState(pos, state.with(SculkShriekerBlock.CAN_SUMMON, true));
-
-                    // swing arm
-                    cir.setReturnValue(ActionResult.SUCCESS);
-                }
-                else {
-                    // if shrieker can already summon, ignore click
-                    cir.setReturnValue(ActionResult.CONSUME);
-                }
+    protected void onUseWithItemInject(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ItemActionResult> cir) {
+        // check if rule is enabled, item is echo shard, and is being used on a sculk shrieker with state can_summon=false
+        if(MiniTweaksSettings.echoShardsEnableShriekers && stack.isOf(Items.ECHO_SHARD) && state.isOf(Blocks.SCULK_SHRIEKER) && !state.get(SculkShriekerBlock.CAN_SUMMON)) {
+            // remove echo shard if in survival
+            if(!player.getAbilities().creativeMode) {
+                stack.decrement(1);
             }
+            // set can_summon state to true and play warden roar
+            world.setBlockState(pos, state.with(SculkShriekerBlock.CAN_SUMMON, true));
+            world.playSound(player, pos, SoundEvents.ENTITY_WARDEN_ROAR, SoundCategory.BLOCKS);
+
+            // swing arm
+            cir.setReturnValue(ItemActionResult.SUCCESS);
         }
     }
 }
