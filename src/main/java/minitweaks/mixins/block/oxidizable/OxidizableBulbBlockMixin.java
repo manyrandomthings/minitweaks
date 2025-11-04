881290de66df6b86c41c48c974c5f6fab830f06f
kimmie-sweet <kimmie-sweet@pm.me>
2025-11-04 19:42:57 +0100

    Update OxidizableBulbBlockMixin.java

--- src/main/java/minitweaks/mixins/block/oxidizable/OxidizableBulbBlockMixin.java
+++ src/main/java/minitweaks/mixins/block/oxidizable/OxidizableBulbBlockMixin.java
@@ -1,1 +1,22 @@
+package minitweaks.mixins.block.oxidizable;
 
+import minitweaks.MiniTweaksSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Oxidizable;
+import net.minecraft.block.OxidizableBulbBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OxidizableBulbBlock.class)
+public abstract class OxidizableBulbBlockMixin {
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        applyOxidation(state, world, pos, random, ci);
+    }
+}
