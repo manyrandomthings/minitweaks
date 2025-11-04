0ac661e8e17e481ea1be5d740ccef77c41e75b47
kimmie-sweet <kimmie-sweet@pm.me>
2025-11-04 19:58:41 +0100

    Update OxidationHelper.java

--- src/main/java/minitweaks/mixins/block/oxidizable/OxidationHelper.java
+++ src/main/java/minitweaks/mixins/block/oxidizable/OxidationHelper.java
@@ -4,6 +4,7 @@ import minitweaks.MiniTweaksSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Oxidizable;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
