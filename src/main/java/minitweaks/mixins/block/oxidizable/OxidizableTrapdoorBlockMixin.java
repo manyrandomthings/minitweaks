0a192b7edccf87b5dce7605f4e400a8adbb7e229
kimmie-sweet <kimmie-sweet@pm.me>
2025-11-04 19:45:52 +0100

    Update OxidizableTrapdoorBlockMixin.java

--- src/main/java/minitweaks/mixins/block/oxidizable/OxidizableTrapdoorBlockMixin.java
+++ src/main/java/minitweaks/mixins/block/oxidizable/OxidizableTrapdoorBlockMixin.java
@@ -1,1 +1,22 @@
+package minitweaks.mixins.block.oxidizable;
 
+import minitweaks.MiniTweaksSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Oxidizable;
+import net.minecraft.block.OxidizableTrapdoorBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OxidizableTrapdoorBlock.class)
+public abstract class OxidizableTrapdoorBlockMixin {
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        applyOxidation(state, world, pos, random, ci);
+    }
+}
