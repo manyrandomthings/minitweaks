4f28dbeb572de90e9df91cd16784bec40adc08a5
kimmie-sweet <kimmie-sweet@pm.me>
2025-11-08 19:56:19 +0100

    Create OxidizablePaneBlockMixin.java

---
+++ src/main/java/minitweaks/mixins/block/oxidizable/OxidizablePaneBlockMixin.java
@@ -0,0 +1,23 @@
+package minitweaks.mixins.block.oxidizable;
+
+import minitweaks.MiniTweaksSettings;
+import minitweaks.util.OxidationHelper;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Oxidizable;
+import net.minecraft.block.OxidizablePaneBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OxidizablePaneBlock.class)
+public abstract class OxidizablePaneBlockMixin {
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        OxidationHelper.applyOxidation(state, world, pos, random, ci);
+    }
+}
